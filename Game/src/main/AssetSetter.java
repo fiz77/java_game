3d21124e5bb599f392f9a9140313a5c257d0106f
fiz77 <m.hafidz.h@students.esqbs.ac.id>
2025-07-18 23:33:36 +0700

    Add files via upload

---
+++ Game/src/main/AssetSetter.java
@@ -0,0 +1,44 @@
+package main;
+
+import object.object_chest;
+import object.object_door;
+import object.object_key;
+
+public class AssetSetter {
+
+	GamePanel gp;
+	
+	public AssetSetter(GamePanel gp) {
+		this.gp = gp;
+	}
+	
+	public void setObject() {
+		gp.obj[0] = new object_key();
+		gp.obj[0].worldX = 23 * gp.tileSize;
+		gp.obj[0].worldY = 7 * gp.tileSize;
+		
+		gp.obj[1] = new object_key();
+		gp.obj[1].worldX = 23 * gp.tileSize;
+		gp.obj[1].worldY = 40 * gp.tileSize;
+		
+		gp.obj[2] = new object_key();
+		gp.obj[2].worldX = 38 * gp.tileSize;
+		gp.obj[2].worldY = 8 * gp.tileSize;	
+		
+		gp.obj[3] = new object_door();
+		gp.obj[3].worldX =  10 * gp.tileSize;
+		gp.obj[3].worldY =  11 * gp.tileSize;	
+		
+		gp.obj[4] = new object_door();
+		gp.obj[4].worldX =  8 * gp.tileSize;
+		gp.obj[4].worldY =  28 * gp.tileSize;
+		
+		gp.obj[5] = new object_door();
+		gp.obj[5].worldX =  12 * gp.tileSize;
+		gp.obj[5].worldY =  22 * gp.tileSize;
+		
+		gp.obj[6] = new object_chest();
+		gp.obj[6].worldX =  10 * gp.tileSize;
+		gp.obj[6].worldY =  7 * gp.tileSize;
+	}
+}
---
+++ Game/src/object/object_door.java
@@ -0,0 +1,20 @@
+package object;
+
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class object_door extends SuperObject{
+
+	public object_door() {
+		name = "Door";
+		try {
+			image = ImageIO.read(getClass().getResourceAsStream("/objects/door.png"));
+			
+		}catch (IOException e){
+			e.printStackTrace();
+		}
+		
+		collision = true;
+	}
+}
